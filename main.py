690799ece31b120b143e54bca80f078d781aae68
Acácio <99909819+acaciobjr@users.noreply.github.com>
2023-12-26 13:30:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import time
 
 def encontrar_diretorio(diretorioOrigem, pasta_inicial='C:\\'):
     for pasta_atual, subpastas, arquivos in os.walk(pasta_inicial):
@@ -12,27 +13,42 @@ def encontrar_diretorio(diretorioOrigem, pasta_inicial='C:\\'):
 while True:
     diretorioOrigem = input("qual o nome do diretório que você deseja fazer backup?")
     print(f"Procurando por um diretório '{diretorioOrigem}' na sua máquina")
+    tempoInicio = time.time()
     resultado = encontrar_diretorio(diretorioOrigem)
+    tempoFim = time.time()
 
     if resultado:
-        print(f"O diretório encontrado: {resultado}")
+        tempoDecorrido = tempoFim - tempoInicio
+        print(f"Foi necessário {tempoDecorrido:.4f} segundos para encontrar o diretório: {resultado}")
         break
     else:
         print(f"{diretorioOrigem} não foi encontrado neste computador. Tente novamente.")
 
-caminho_do_desktop = os.path.expanduser('~/Desktop')
-pasta='backup'
-caminhoCompleto = os.path.join(caminho_do_desktop, pasta)
-os.makedirs(caminhoCompleto)
-
 Listagem = os.listdir(resultado)
 quant = len(Listagem)
-print(f'{quant} itens no diretório')
+
+caminho_do_desktop = os.path.expanduser('~/Desktop')
+pasta='backup'
+contador=2
+while True:
+    nome_pasta = f"{pasta} ({contador})"
+    caminhoCompleto = os.path.join(caminho_do_desktop, nome_pasta)
+    if not os.path.exists(caminhoCompleto):
+        os.makedirs(caminhoCompleto)
+        break
+    contador += 1
+print(f"pasta '{nome_pasta} criada com sucesso")
 
 for arquivo in Listagem:
     caminhoInicial = os.path.join(resultado, arquivo)
     caminhoFinal = os.path.join(caminhoCompleto, arquivo)
     shutil.move(caminhoInicial, caminhoFinal)
+
+Listagem2 = os.listdir(caminhoCompleto)
+quant2 = len(Listagem2)
+if quant2 == quant:
+    print(f'{quant} itens em {resultado} e {quant2} em {nome_pasta}.')
+    time.sleep(2)
     print('backup realizado com sucesso.')
 
 ...

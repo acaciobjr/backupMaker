2a5063b65d2824bac39d65961d79aa8637f1a3e9
Acácio <99909819+acaciobjr@users.noreply.github.com>
2024-02-23 15:17:56 -0300

    Update zipSend.py

--- send.py
+++ send.py
@@ -1,45 +1,15 @@
-import smtplib
-import zipfile
-import os
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
+import yagmail
 
-from_email = input('digite seu email: ')
-to_email = input('digite o email destinatário: ')
-password = input('digite sua senha: ')
+destinatario = 'email de destino'
+caminho_zip = 'caminho do seu arquivo.zip'
+usuario_gmail = 'email remetente'
+senha = 'senha do email remetente'
 
-#caminho do diretório que deseja enviar
-folder_path = input('insira o caminho da pasta que deseja enviar: ')
-
-zip_filename = 'backup.zip'
-
-with open(zip_filename, 'rb') as attachment:
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(attachment.read())
-
-#configuração do e-mail
-msg = MIMEMultipart()
-msg['From'] = from_email
-msg['To'] = to_email
-msg['Subject'] = 'Assunto do E-mail'
-
-#anexando arquivo zip a email
-attachment = open(zip_filename, 'rb')
-part = MIMEBase('application', 'octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % zip_filename)
-msg.attach(part)
-
-try:
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(from_email, password)
-    text = msg.as_string()
-    server.sendmail(from_email, to_email, text)
-    server.quit()
-except Exception as e:
-    print("Erro ao conectar no servidor: ", e)
-
-#os.remove(zip_filename)
+def enviando_zip_com_yag(destinatario, caminho_zip, usuario_gmail, senha):
+    yag = yagmail.SMTP(usuario_gmail, senha)
+    yag.send(
+        to=destinatario,
+        subject="assunto do email",
+        contents="texto da mensagem",
+        attachments= caminho_zip,
+    )
